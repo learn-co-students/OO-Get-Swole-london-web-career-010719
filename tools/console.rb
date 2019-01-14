e831804b69d6c02b4d2afd19131d58d387ead4b8
studioshane <studioshane@gmail.com>
2019-01-14 18:48:53 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,20 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+lifter1 = Lifter.new("bob", 150)
+lifter2 = Lifter.new("dave", 160)
+lifter3 = Lifter.new("bill", 210)
+
+gym1 = Gym.new("croydon")
+gym2 = Gym.new("streatham")
+gym3 = Gym.new("moorgate")
+gym4 = Gym.new("old street")
+
+membership1 = Membership.new(5, lifter1, gym1)
+membership2 = Membership.new(10, lifter2, gym2)
+membership3 = Membership.new(15, lifter3, gym4)
+membership4 = Membership.new(10, lifter1, gym3)
+membership5 = Membership.new(55, lifter2, gym3)
 
 binding.pry
 
