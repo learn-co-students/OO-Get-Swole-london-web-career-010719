eff0e43bc0ccd58942dfc6aadad19ec32818479a
kkoomin <minha.koo.9@gmail.com>
2019-01-15 08:18:36 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,29 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters
+    memberships.map {|membership| membership.lifter}
+  end
+
+  def lifters_name
+    lifters.map {|lifter| lifter.name}
+  end
+
+  def lifter_total
+    lifters.inject(0) {|sum, lifter| sum + lifter.lift_total}
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    memberships.map {|membership| membership.gym}
+  end
+
+  def self.average_lift_total
+    (all.inject(0) {|sum, lifter| sum + lifter.lift_total}.to_f / all.count.to_f).round(2)
+  end
+
+  def total_cost_of_gym_membership
+    memberships.inject(0) {|sum, membership| sum + membership.cost}
+  end        
+
+
+  def sign_up_new_gym(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
+
 end
